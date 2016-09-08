8a0d5af43b3e0bcb4787255057a54855fed4b2c7
shyamahira <shyamahira@bbtower.co.jp>
2016-09-08 22:35:19 +0900

    letメソッドとFactory Girl 終了

--- spec/factories/customers.rb
+++ spec/factories/customers.rb
@@ -1,6 +1,8 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
 FactoryGirl.define do
   factory :customer do
+    family_name '山田'
+    given_name '太郎'
+    family_name_kana 'ヤマダ'
+    given_name_kana 'タロウ'
   end
 end
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,14 +1,21 @@
 require 'rails_helper'
 
 describe Customer do
+  let(:customer) { FactoryGirl.build(:customer) }
+
+  example '妥当なオブジェクト' do
+    expect(customer).to be_valid
+  end
+
   %w{family_name given_name family_name_kana given_name_kana}.each do |column_name|
     example "#{column_name} は 空であってはならない" do
-      customer = Customer.new(
-        family_name: '', given_name: '太郎',
-        family_name_kana: 'ヤマダ', given_name_kana: 'タロウ'
-      )
-
       customer[column_name] = ''
+      expect(customer).not_to be_valid
+      expect(customer.errors[column_name]).to be_present
+    end
+
+    example "#{column_name} は40文字以内" do
+      customer[column_name] = 'ア' * 41
       expect(customer).not_to be_valid
       expect(customer.errors[column_name]).to be_present
     end
