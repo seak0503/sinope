abf0c02851f2684d0662ef7ea10b5aa576caf327
shyamahira <shyamahira@bbtower.co.jp>
2016-09-30 00:31:31 +0900

    ポイントシステム(3) -- timecop 完了

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -32,7 +32,19 @@ class Customer < ActiveRecord::Base
     def authenticate(username, password)
       customer = find_by(username: username)
       if customer.try(:password_digest) && BCrypt::Password.new(customer.password_digest) == password
-        customer.rewards.create(points: 1)
+        Time.zone = 'Tokyo'
+        now = Time.current
+        if now.hour < 5
+          time0 = now.yesterday.midnight.advance(hours: 5)
+          time1 = now.midnight.advance(hours: 5)
+        else
+          time0 = now.midnight.advance(hours: 5)
+          time1 = now.tomorrow.midnight.advance(hours: 5)
+        end
+
+        unless customer.rewards.where(created_at: time0...time1).exists?
+          customer.rewards.create(points: 1)
+        end
         customer
       else
         nil
