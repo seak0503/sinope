2382c186a19f0e5e3e2b27ae1272e8af941295a3
shyamahira <shyamahira@bbtower.co.jp>
2016-10-02 07:20:28 +0900

    ポイントシステム(4) -- サービスオブジェクト 完了

---
+++ spec/services/reward_manager_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+feature RewardManager do
+  context '#grant_login_points' do
+    let(:customer) { create(:customer) }
+
+    example "日付変更時刻をまたいで2回ログインすると、ユーザーの保有ポイントが2増える" do
+      Time.zone = 'Tokyo'
+      date_boundary = Time.zone.local(2013, 1, 1, 5, 0, 0)
+      expect {
+        Timecop.freeze(date_boundary.advance(seconds: -1))
+        RewardManager.new(customer).grant_login_points
+        Timecop.freeze(date_boundary)
+        RewardManager.new(customer).grant_login_points
+      }.to change { customer.points }.by(2)
+    end
+
+    example "日付変更時刻をまたがずに2回ログインしても、ユーザーの保有ポイントは1しか増えない" do
+      Time.zone = 'Tokyo'
+      date_boundary = Time.zone.local(2013, 1, 1, 5, 0, 0)
+      expect {
+        Timecop.freeze(date_boundary)
+        RewardManager.new(customer).grant_login_points
+        Timecop.freeze(date_boundary.advance(hours: 24, seconds: -1))
+        RewardManager.new(customer).grant_login_points
+      }.to change { customer.points }.by(1)
+    end
+  end
+end
