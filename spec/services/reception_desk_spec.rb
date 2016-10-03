8a6c4dbbd2f59dbe6ad8de5a8d18fa5632913178
shyamahira <shyamahira@bbtower.co.jp>
2016-10-03 21:46:31 +0900

    ポイントシステム(6) -- Message Expectation 完了

--- spec/services/reception_desk_spec.rb
+++ spec/services/reception_desk_spec.rb
@@ -25,14 +25,24 @@ describe ReceptionDesk do
       expect(result).to be_nil
     end
 
-    example "ログインに成功すると、ユーザーの保有ポイントが1増える" do
+    #example "ログインに成功すると、ユーザーの保有ポイントが1増える" do
       #pending 'Customer#pointsが未実装'
       #customer.stub(:points).and_return(0)
       #上記スタブメソッドはrspec3では利用できないため、下記のように書き換える
       #allow(customer).to receive(:points).and_return(0)
-      expect {
-        ReceptionDesk.new(customer.username, 'correct_password').sign_in
-      }.to change { customer.points }.by(1)
+    #  expect {
+    #    ReceptionDesk.new(customer.username, 'correct_password').sign_in
+    #  }.to change { customer.points }.by(1)
+    #end
+
+    example "ログインに成功すると、RewardManager#grant_login_pointsが呼ばれる" do
+      expect_any_instance_of(RewardManager).to receive(:grant_login_points)
+      ReceptionDesk.new(customer.username, 'correct_password').sign_in
+    end
+
+    example "ログインに失敗すると、RewardManager#grant_login_pointsは呼ばれない" do
+      expect_any_instance_of(RewardManager).not_to receive(:grant_login_points)
+      ReceptionDesk.new(customer.username, 'wrong_password').sign_in
     end
   end
 end
