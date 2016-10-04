ae256ea8a1c0550db26904a1424e6e6360b10d02
shyamahira <shyamahira@bbtower.co.jp>
2016-10-05 07:06:58 +0900

    ポイントシステム(8) -- データベーストランザクション 完了

--- app/services/reward_manager.rb
+++ app/services/reward_manager.rb
@@ -16,9 +16,11 @@ class RewardManager
       time1 = now.tomorrow.midnight.advance(hours: 5)
     end
 
-    unless customer.rewards.where(created_at: time0...time1).exists?
-      customer.rewards.create(points: 1)
-      customer.rewards.create(points: 2) if time0.wday == 6
+    ActiveRecord::Base.transaction do
+      unless customer.rewards.where(created_at: time0...time1).exists?
+        customer.rewards.create(points: 1)
+        customer.rewards.create(points: 2) if time0.wday == 6
+      end
     end
   end
 end
